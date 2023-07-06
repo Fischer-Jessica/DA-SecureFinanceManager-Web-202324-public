48c4cfb49cbd3d62b7917fa652fb120c8d8d8e29
Fischer <Jessica.Fischer@it.htlhl.at>
2023-07-06 12:29:44 +0200

    added a basic sign-up as well as a sign-in component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {SignUpComponent} from "./sign-up/sign-up.component";
+import { SignInComponent } from './sign-in/sign-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignUpComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule
