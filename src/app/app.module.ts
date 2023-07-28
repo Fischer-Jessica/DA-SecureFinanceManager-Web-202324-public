d567917bce7272e53f0293960eaecc52b3c238a6
Fischer <Jessica.Fischer@it.htlhl.at>
2023-07-28 14:46:47 +0200

    added a card to the logged-in-overview

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,31 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
-import {AppRoutingModule} from './app-routing.module'; // Import the AppRoutingModule
+// own Components
 import {AppComponent} from './app.component';
+import {AuthenticationComponent} from './authentication/authentication.component';
 import {SignUpComponent} from "./sign-up/sign-up.component";
 import {SignInComponent} from './sign-in/sign-in.component';
-import {AuthenticationComponent} from './authentication/authentication.component';
+import {LoggedInHomepageComponent} from './logged-in-homepage/logged-in-homepage.component';
+import {LoggedInHomepageNavbarComponent} from './logged-in-homepage-navbar/logged-in-homepage-navbar.component';
+import {LoggedInOverviewComponent} from './logged-in-overview/logged-in-overview.component';
 import {ImprintComponent} from './imprint/imprint.component';
+
+// routing - imports
+import {AppRoutingModule} from './app-routing.module'; // Import the AppRoutingModule
+// forms - imports
 import {FormsModule} from '@angular/forms';
+
 import {HttpClient, HttpClientModule} from '@angular/common/http';
 import {CommonModule} from "@angular/common";
-import {LoggedInHomepageNavbarComponent} from './logged-in-homepage-navbar/logged-in-homepage-navbar.component';
 
+// Translation - imports
 import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
 import {TranslateHttpLoader} from '@ngx-translate/http-loader';
-import {LoggedInHomepageComponent} from './logged-in-homepage/logged-in-homepage.component';
-import {LoggedInOverviewComponent} from './logged-in-overview/logged-in-overview.component';
+
+// Design - imports
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from "@angular/material/button";
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http);
@@ -37,6 +47,7 @@ export function HttpLoaderFactory(http: HttpClient) {
     FormsModule,
     HttpClientModule,
     CommonModule,
+    MatCardModule,
     TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
@@ -44,7 +55,9 @@ export function HttpLoaderFactory(http: HttpClient) {
         deps: [HttpClient]
       }
     }),
-    AppRoutingModule // Add the AppRoutingModule to the imports array
+    AppRoutingModule,
+    MatButtonModule,
+    // Add the AppRoutingModule to the imports array
   ],
   providers: [],
   bootstrap: [AppComponent]
