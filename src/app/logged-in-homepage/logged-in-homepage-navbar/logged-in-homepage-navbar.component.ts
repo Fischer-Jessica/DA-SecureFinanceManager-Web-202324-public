a176a69bab73e3e57401621df0cefc2cf33296eb
Fischer <Jessica.Fischer@it.htlhl.at>
2023-09-18 18:44:27 +0200

    fixing the problem with the routing of the logged-in-homepage-navbar and changing its design

--- src/app/logged-in-homepage/logged-in-homepage-navbar/logged-in-homepage-navbar.component.ts
+++ src/app/logged-in-homepage/logged-in-homepage-navbar/logged-in-homepage-navbar.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'logged-in-homepage-navbar',
@@ -6,5 +7,22 @@ import {Component} from '@angular/core';
   styleUrls: ['./logged-in-homepage-navbar.component.css']
 })
 export class LoggedInHomepageNavbarComponent {
+  constructor(private router: Router) {
+  }
 
+  goToOverview() {
+    this.router.navigateByUrl('/logged-in-homepage/logged-in-overview');
+  }
+
+  goToCategories() {
+    this.router.navigateByUrl('/logged-in-homepage/logged-in-categories');
+  }
+
+  goToLabels() {
+    this.router.navigateByUrl('/logged-in-homepage/logged-in-labels');
+  }
+
+  goToDiagrams() {
+    this.router.navigateByUrl('/logged-in-homepage/logged-in-diagrams');
+  }
 }
