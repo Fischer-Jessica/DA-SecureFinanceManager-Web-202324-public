1efa453bae2b0c48ba3ba465ae45958c743bc12c
Fischer <Jessica.Fischer@it.htlhl.at>
2024-01-31 19:00:20 +0100

    adding that the chosen list-item of the navbar is highlighted

--- src/app/views/logged-in-homepage/navbar/navbar.component.ts
+++ src/app/views/logged-in-homepage/navbar/navbar.component.ts
@@ -7,4 +7,10 @@ import {Router} from "@angular/router";
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  constructor(private router: Router) {
+  }
+
+  isActive(route: string): boolean {
+    return this.router.url === route;
+  }
 }
