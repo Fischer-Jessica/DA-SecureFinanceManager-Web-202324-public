ca1dc4e11407b939ecbf80304e823da02b7b3618
Fischer <Jessica.Fischer@it.htlhl.at>
2024-02-21 20:15:31 +0100

    adding documentation to the navbar.component and improving the design for smaller screens

--- src/app/views/logged-in-homepage/navbar/navbar.component.ts
+++ src/app/views/logged-in-homepage/navbar/navbar.component.ts
@@ -6,10 +6,27 @@ import {Router} from "@angular/router";
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
+
+/**
+ * Component for the navigation bar in the logged-in homepage.
+ * Responsible for displaying navigation links and highlighting the active link.
+ * @class NavbarComponent
+ * @author Fischer
+ * @fullName Fischer, Jessica Christina
+ */
 export class NavbarComponent {
+  /**
+   * Creates an instance of NavbarComponent.
+   * @param {Router} router - The Router service for navigation.
+   */
   constructor(private router: Router) {
   }
 
+  /**
+   * Checks if the current route matches the given route.
+   * @param route - The route to check against.
+   * @returns {boolean} - True if the current route matches the given route, false otherwise.
+   */
   isActive(route: string): boolean {
     return this.router.url === route;
   }
