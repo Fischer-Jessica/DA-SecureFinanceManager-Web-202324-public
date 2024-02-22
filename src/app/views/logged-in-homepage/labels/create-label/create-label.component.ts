1a8b4a98491878373ec6be9a0ba67d27b52a9aae
Fischer <Jessica.Fischer@it.htlhl.at>
2024-02-22 09:41:10 +0100

    adding documentation to the create-label.component, outsourcing the design of the form into a global file, improving the usage of alerts, translation and console-outputs

--- src/app/views/logged-in-homepage/labels/create-label/create-label.component.ts
+++ src/app/views/logged-in-homepage/labels/create-label/create-label.component.ts
@@ -3,49 +3,94 @@ import {Label} from '../../../../logic/models/Label';
 import {LabelService} from '../../../../logic/services/LabelService';
 import {Router} from '@angular/router';
 import {LocalStorageService} from '../../../../logic/LocalStorageService';
+import {SnackBarService} from "../../../../logic/services/SnackBarService";
+import {TranslateService} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-create-new-label',
   templateUrl: './create-label.component.html',
-  styleUrls: ['./create-label.component.css']
+  styleUrls: ['./create-label.component.css', '../../logged-in-homepage.component.css']
 })
+/**
+ * Component for creating a new label
+ * @class CreateLabelComponent
+ * @author Fischer
+ * @fullName Fischer, Jessica Christina
+ */
 export class CreateLabelComponent {
+  /**
+   * The label object to store label details
+   * @type {Label}
+   */
   label: Label = {
     labelName: '',
     labelColourId: 0,
   };
 
+  /**
+   * Constructor for CreateLabelComponent
+   * @param labelService The service for label operations
+   * @param router The Angular router service
+   * @param localStorageService The service for managing local storage
+   * @param translationService The service for translation
+   * @param snackBarService The service for displaying snack bar messages
+   * @memberOf CreateLabelComponent
+   */
   constructor(private labelService: LabelService,
               private router: Router,
-              private localStorageService: LocalStorageService) {
+              private localStorageService: LocalStorageService,
+              private translationService: TranslateService,
+              private snackBarService: SnackBarService) {
   }
 
+  /**
+   * Callback function invoked when a colour is selected
+   * @param colourId The ID of the selected colour
+   * @memberOf CreateLabelComponent
+   */
   onColourSelected(colourId: number): void {
     this.label.labelColourId = colourId;
   }
 
+  /**
+   * Method to handle form submission
+   * @param formData The form data submitted
+   * @memberOf CreateLabelComponent
+   */
   onSubmit(formData: Label) {
     formData.labelColourId = this.label.labelColourId;
     const storedUser = this.localStorageService.getItem('loggedInUser');
 
+    if (formData.labelName === '' || formData.labelColourId === 0) {
+      this.snackBarService.showAlert(this.translationService.instant('logged-in-homepage.labels.create-label.alert_missing_fields'));
+      return;
+    }
+
     if (!storedUser) {
-      console.error('User is not logged in');
+      this.snackBarService.showAlert(this.translationService.instant('authentication.alert_user_not_logged_in'));
+      this.router.navigateByUrl('/authentication/login');
       return;
     }
 
     const loggedInUser = JSON.parse(storedUser);
 
-    // Hier die Anpassungen für den Label-Service vornehmen
     this.labelService.insertLabel(loggedInUser.username, loggedInUser.password, formData)
       .subscribe({
         next: (response) => {
           this.router.navigateByUrl('/logged-in-homepage/labels');
         },
         error: (err) => {
-          console.log(err);
+          if (err.status === 401) {
+            this.snackBarService.showAlert(this.translationService.instant('authentication.alert_user_not_logged_in'));
+            this.localStorageService.removeItem('loggedInUser');
+            this.router.navigateByUrl('/authentication/login');
+          } else if (err.status === 400) {
+            this.snackBarService.showAlert(this.translationService.instant('logged-in-homepage.labels.create-label.alert_missing_fields'));
+          } else {
+            this.snackBarService.showAlert(this.translationService.instant('alert_error'));
+            console.error(this.translationService.instant('logged-in-homepage.label.console_error_creating_label'), err);
+          }
         }
       });
   }
-
-  // Weitere Methoden wie returnToLabels können hier hinzugefügt werden
 }
